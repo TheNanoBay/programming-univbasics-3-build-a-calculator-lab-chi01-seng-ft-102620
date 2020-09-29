a3f1f4ab3f1ab564448d595ae7c2f6b354a412c1
Mehdi A Noorani <noorani.mehdi@outlook.com>
2020-09-29 15:58:41 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,12 +4,12 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-  total = num2 - num1
+  total = num1 - num2
   return total 
 end
 
 def division(num1, num2)
-  total = num2 / num1
+  total = num1 / num2
   return total 
 end
 
@@ -19,7 +19,7 @@ def multiplication(num1, num2)
 end
 
 def modulo(num1, num2)
-  total = num2 % num1 
+  total = num1 % num2 
   return total 
 end
 
